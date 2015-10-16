8a9cacb0609a46e58987839b1bb38a654be149ad
Aleix Solanes <al.solanes@gmail.com>
2015-10-16 16:25:18 +0200

    FInal format

--- freeling_functions.py
+++ freeling_functions.py
@@ -1,12 +1,22 @@
+# Code developed by Pablo Martinez and Aleix Solanes
+# MAI - UPC - 2015
+
 import subprocess
 
-# Tokenize a file with a list of properties (word, lemma, category of the word, probability assignment). By default considers the input to be in English, but if specified as a parameter can be set to another language like spanish('es')"
+
 def tokenize_file(path_to_file, lang='en'):
+    '''
+    Requires the path to the input file, and optionally the language used.
+    Tokenize a file with a list of properties (word, lemma, category of the word, probability assignment). By default considers the input to be in English, but if specified as a parameter can be set to another language like spanish('es')"
+    '''
     p = subprocess.Popen('analyze -f ' + lang + '.cfg <'+path_to_file+'', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     return to_list(p)
 
-# Given an input file, returns a list of words and the type of word it is
 def extract_type(path_to_file, lang='en'):
+    '''
+    Requires the path to the input file, and optionally the language used.
+    Given an input file, returns a list of words and the type of word it is
+    '''
     list_out = tokenize_file(path_to_file, lang)
     res = []
     for a in range(len(list_out[:])):
@@ -14,8 +24,12 @@ def extract_type(path_to_file, lang='en'):
             res.append([list_out[a][0],list_out[a][2]])
     return res
 
-#Given an input file, returns a list of words and the phonetics of the word
+#
 def extract_phonetics(path_to_file, lang='en'):
+    '''
+    Requires the path to the input file, and optionally the language used.
+    Given an input file, returns a list of words and the phonetics of the word
+    '''
     list_out = subprocess.Popen('analyze -f ' + lang + '.cfg --phon <'+path_to_file+'', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     list_out = to_list(list_out)
     res = []
@@ -23,8 +37,12 @@ def extract_phonetics(path_to_file, lang='en'):
         if len(list_out[a])>2:
             res.append([list_out[a][0],list_out[a][1]])
     return res
-#Assign probabilities for each analysis of given word
+
 def extract_probabilities(path_to_file, lang='en'):
+    '''
+    Requires the path to the input file, and optionally the language used.
+    Assign probabilities for each analysis of given word
+    '''
     list_out = tokenize_file(path_to_file, lang)
     res = []
     for a in range(len(list_out[:])):
@@ -32,8 +50,12 @@ def extract_probabilities(path_to_file, lang='en'):
             res.append([list_out[a][0],list_out[a][3]])
     return res
 
-#Returns a list of names identified by freeling
+#
 def get_names(path_to_file, lang='en'):
+    '''
+    Requires the path to the input file, and optionally the language used.
+    Returns a list of names identified by freeling. 
+    '''
     list_types = extract_type(path_to_file)
     res = []
     for a in range(len(list_types[:])):
@@ -41,8 +63,10 @@ def get_names(path_to_file, lang='en'):
             res.append(list_types[a][0])    
     return res
     
-# Given an input, returns the output as a structured list.
 def to_list(p):
+    '''
+    Given an input, returns the output as a structured list. It is used from other functions
+    '''
     res = []
     for line in p.stdout.readlines():
             aux = []
@@ -54,4 +78,3 @@ def to_list(p):
                 res.append(aux)  
     return res
             
-#print tokenize_file('input.txt')
